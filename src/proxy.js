297ceba3f2d6ccddaeb6844c25d969d9ded527eb
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-06-29 22:04:09 +0600

    FIx bug

--- src/proxy.js
+++ src/proxy.js
@@ -1,22 +1,47 @@
+// import { NextResponse } from "next/server";
+
+import { auth } from "./app/lib/auth";
+
+// export async function proxy(request) {
+//   const sessionToken = request.cookies.get("better-auth.session_token");
+//   if (!sessionToken) {
+//     const loginUrl = new URL("/auth/login", request.url);
+
+//     loginUrl.searchParams.set("callbackUrl", request.nextUrl.pathname);
+
+//     return NextResponse.redirect(loginUrl);
+//   }
+
+//   return NextResponse.next();
+
+// }
+// export const config = {
+//   matcher: "/properties/:path+",
+// };
 import { NextResponse } from "next/server";
 
 export async function proxy(request) {
-  const sessionToken = request.cookies.get("better-auth.session_token");
-  if (!sessionToken) {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+
+  if (!session) {
     const loginUrl = new URL("/auth/login", request.url);
 
-    loginUrl.searchParams.set("callbackUrl", request.nextUrl.pathname);
+    loginUrl.searchParams.set("next", request.nextUrl.pathname);
 
     return NextResponse.redirect(loginUrl);
   }
 
   return NextResponse.next();
-  //   if (!sessionToken) {
-  //     return NextResponse.redirect(new URL("/auth/login", request.url));
-  //   }
-
-  //   return NextResponse.next();
 }
+
 export const config = {
-  matcher: "/properties/:path+",
+  matcher: [
+    "/properties/:path",
+    "/my-ideas",
+    "/add-idea",
+    "/my-interactions",
+    "/dashboard",
+  ],
 };
